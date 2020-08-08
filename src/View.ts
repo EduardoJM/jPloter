d14d0ada5e543aa2015b3b814344833b63e54ddc
EduardoJM <eduardo_y05@outlook.com>
2020-08-08 15:32:09 -0300

    feat: added renderbegin event handler in the View

--- src/View.ts
+++ src/View.ts
@@ -55,6 +55,8 @@ export class View {
      */
     renderList: RenderItem[];
 
+    private onRenderBeginEventHandlers: ((context: CanvasRenderingContext2D) => void)[];
+
     /**
      * creates a new instance of View.
      * @param canvas the HTMLCanvasElement to render.
@@ -76,6 +78,8 @@ export class View {
         };
         this.items = [];
         this.renderList = [];
+
+        this.onRenderBeginEventHandlers = [];
     }
 
     /**
@@ -84,6 +88,9 @@ export class View {
     render(): void {
         this.context.resetTransform();
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.onRenderBeginEventHandlers.forEach((fn) => {
+            fn.call(this, this.context);
+        });
 
         const screenBounds = { left: 0, top: 0, right: this.canvas.width, bottom: this.canvas.height };
         // indicate to items if bounding calculation is needed
@@ -145,5 +152,18 @@ export class View {
             return null;
         }
         return filtered[0];
+    }
+
+    addEventListener(event: 'renderbegin', fn: (context: CanvasRenderingContext2D) => void): void {
+        if (event === 'renderbegin') {
+            this.onRenderBeginEventHandlers.push(fn);
+        }
+    }
+
+    removeEventListener(event: 'renderbegin', fn: (context: CanvasRenderingContext2D) => void): void {
+        if (event === 'renderbegin') {
+            const idx = this.onRenderBeginEventHandlers.indexOf(fn);
+            this.onRenderBeginEventHandlers.splice(idx, 1);
+        }
     }
 }
