cca9c639465211402a617e957d7df06948f8cba8
EduardoJM <eduardo_y05@outlook.com>
2020-09-25 17:06:16 -0300

    test: rewrite the Color tests

--- src/Style/Color.test.ts
+++ src/Style/Color.test.ts
@@ -1,54 +1,167 @@
 import { Color } from './Color';
+import ColorNames from './ColorNames';
 
-test('Color.fromString with names must be correct', () => {
-    const skyBlue = Color.fromString('SkyBlue');
-    expect(skyBlue.r).toBe(135);
-    expect(skyBlue.g).toBe(206);
-    expect(skyBlue.b).toBe(235);
-    const oliveDrab = Color.fromString('OliveDrab');
-    expect(oliveDrab.r).toBe(107);
-    expect(oliveDrab.g).toBe(142);
-    expect(oliveDrab.b).toBe(35);
+test('Empty Color instantiated must be black', () => {
+    const c = new Color();
+    expect(c.r).toBe(0);
+    expect(c.g).toBe(0);
+    expect(c.b).toBe(0);
 });
 
-test('Color.fromRgb must be correct', () => {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    const color = Color.fromRgb(r, g, b);
-    expect(color.r).toBe(r);
-    expect(color.g).toBe(g);
-    expect(color.b).toBe(b);
+test('Color.fix must be fix the range of color component', () => {
+    const r = Math.floor(Math.random() * 255) + 256; // positive > 255
+    const g = Math.floor(Math.random() * 255); // 0-255
+    const b = Math.floor(Math.random() * 100) - 200; // < 0
+    const c = new Color();
+    c.r = r;
+    c.g = g;
+    c.b = b;
+    expect(c.r).toBe(r);
+    expect(c.g).toBe(g);
+    expect(c.b).toBe(b);
+    c.fix();
+    expect(c.r).toBe(255);
+    expect(c.g).toBe(g);
+    expect(c.b).toBe(0);
 });
 
-test('fix color ranges in Color.fromRgb', () => {
-    const r = 500;
-    const g = Math.floor(Math.random() * 256);
-    const b = -20;
-    const color = Color.fromRgb(r, g, b);
-    expect(color.r).toBe(255);
-    expect(color.g).toBe(g);
-    expect(color.b).toBe(0);
+test('Color.fromRgb must be create a color from rgb fixing ranges', () => {
+    let r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    let c = Color.fromRgb(r, g, b);
+    expect(c.r).toBe(r);
+    expect(c.g).toBe(g);
+    expect(c.b).toBe(b);
+    r = Math.floor(Math.random() * 255) + 256; // > 255
+    b = Math.floor(Math.random() * 100) - 200; // < 0
+    c = Color.fromRgb(r, g, b);
+    expect(c.r).toBe(255);
+    expect(c.g).toBe(g);
+    expect(c.b).toBe(0);
 });
 
-test('Color.fromHex must be correct', () => {
-    // #RRGGBB
-    const slateBlue = Color.fromHex('#6A5ACD');
-    expect(slateBlue.r).toBe(106);
-    expect(slateBlue.g).toBe(90);
-    expect(slateBlue.b).toBe(205);
-    // #RGB
-    const cyan = Color.fromHex('#0FF');
-    expect(cyan.r).toBe(0);
-    expect(cyan.g).toBe(255);
-    expect(cyan.b).toBe(255);
-    // WITHOUT #
-    const mediumAquaMarine = Color.fromHex('66CDAA');
-    expect(mediumAquaMarine.r).toBe(102);
-    expect(mediumAquaMarine.g).toBe(205);
-    expect(mediumAquaMarine.b).toBe(170);
-    const yellow = Color.fromHex('FF0');
-    expect(yellow.r).toBe(255);
-    expect(yellow.g).toBe(255);
-    expect(yellow.b).toBe(0);
+test('Color.toString must be reloaded with fromString', () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    const c1 = Color.fromRgb(r, g, b);
+    const str = c1.toString();
+    const c2 = Color.fromString(str);
+    expect(c2.r).toBe(r);
+    expect(c2.g).toBe(g);
+    expect(c2.b).toBe(b);
+});
+
+test('Color.fromHex can parse colors with # and without it', () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    let rHex = r.toString(16);
+    let gHex = g.toString(16);
+    let bHex = b.toString(16);
+    if (rHex.length === 1) {
+        rHex = `0${rHex}`;
+    }
+    if (gHex.length === 1) {
+        gHex = `0${gHex}`;
+    }
+    if (bHex.length === 1) {
+        bHex = `0${bHex}`;
+    }
+    const hex1 = `#${rHex}${gHex}${bHex}`;
+    const hex2 = `${rHex}${gHex}${bHex}`;
+    const c1 = Color.fromHex(hex1);
+    expect(c1.r).toBe(r);
+    expect(c1.g).toBe(g);
+    expect(c1.b).toBe(b);
+    const c2 = Color.fromHex(hex2);
+    expect(c2.r).toBe(r);
+    expect(c2.g).toBe(g);
+    expect(c2.b).toBe(b);
+});
+
+test('Color.fromHex can parse the FFFFFF or FFF format', () => {
+    const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+    const d1 = digits[Math.floor(Math.random() * digits.length)];
+    const d2 = digits[Math.floor(Math.random() * digits.length)];
+    const d3 = digits[Math.floor(Math.random() * digits.length)];
+    const hex1 = `${d1}${d2}${d3}`;
+    const hex2 = `${d1}${d1}${d2}${d2}${d3}${d3}`;
+    const c1 = Color.fromHex(hex2);
+    const c2 = Color.fromHex(hex1);
+    const c3 = Color.fromHex(`#${hex1}`);
+    expect(c2.r).toBe(c1.r);
+    expect(c2.g).toBe(c1.g);
+    expect(c2.b).toBe(c1.b);
+    expect(c3.r).toBe(c1.r);
+    expect(c3.g).toBe(c1.g);
+    expect(c3.b).toBe(c1.b);
+});
+
+test('Color.fromString must parse rgb(r, g, b) format', () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    const c = `rgb(${r}, ${g}, ${b})`;
+    const cl = Color.fromString(c);
+    expect(cl.r).toBe(r);
+    expect(cl.g).toBe(g);
+    expect(cl.b).toBe(b);
+});
+
+test('Color.fromString must parse color from name', () => {
+    const keys = Object.keys(ColorNames);
+    keys.forEach((cl) => {
+        let name = '';
+        for (let i = 0; i < cl.length; i += 1) {
+            const char = cl[i];
+            if (Math.floor(Math.random() * 100) % 2 === 0) {
+                name = `${name}${char.toLowerCase()}`;
+            } else {
+                name = `${name}${char.toUpperCase()}`;
+            }
+        }
+        const hex = ColorNames[cl];
+        const cl1 = Color.fromHex(hex);
+        const cl2 = Color.fromString(name);
+        expect(cl1.r).toBe(cl2.r);
+        expect(cl1.g).toBe(cl2.g);
+        expect(cl1.b).toBe(cl2.b);
+    });
+});
+
+test('Color.fromString must parse color from hex', () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    let rHex = r.toString(16);
+    let gHex = g.toString(16);
+    let bHex = b.toString(16);
+    if (rHex.length === 1) {
+        rHex = `0${rHex}`;
+    }
+    if (gHex.length === 1) {
+        gHex = `0${gHex}`;
+    }
+    if (bHex.length === 1) {
+        bHex = `0${bHex}`;
+    }
+    const hex1 = `#${rHex}${gHex}${bHex}`;
+    const hex2 = `${rHex}${gHex}${bHex}`;
+    const c1 = Color.fromString(hex1);
+    expect(c1.r).toBe(r);
+    expect(c1.g).toBe(g);
+    expect(c1.b).toBe(b);
+    const c2 = Color.fromString(hex2);
+    expect(c2.r).toBe(r);
+    expect(c2.g).toBe(g);
+    expect(c2.b).toBe(b);
+});
+
+test('Color.fromString with unknown format must be black', () => {
+    const c = Color.fromString('3as234');
+    expect(c.r).toBe(0);
+    expect(c.g).toBe(0);
+    expect(c.b).toBe(0);
 });
